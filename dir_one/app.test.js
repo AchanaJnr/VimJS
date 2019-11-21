5a64e7c2708b876f01926c4224b80e73adf88b83
achanajnr <achanamosey@gmail.com>
2019-11-21 14:20:30 +0000

    Added some lines of code to the app.test.js file

--- dir_one/app.test.js
+++ dir_one/app.test.js
@@ -1,5 +1,9 @@
+// CodeSaitama
+
+// Paasing a function a callback...
 let x = function(a, b){return a * b}
-
 let sampleArr = [3, 7, 10, 14, 2, 5, 1, 9];
-
 console.log(sampleArr.map(x));
+
+let date = new Date();
+console.log(date.toString())
