79a39754c5d9ddce70e46419ee1b404e01557fdb
codesaitama <achanamosey@gmail.com>
2019-11-28 10:37:35 +0000

    Invoked the codeGenerator function...

--- functions.js/app.js
+++ functions.js/app.js
@@ -14,3 +14,5 @@ console.log(moneyFormat(moneyNum, 'en', 2));
 
 // Code Generator...
 let codeGenerator = (str, len) => `${str}-${Math.random().toString(36).substr(2, len).toUpperCase()}`;
+
+console.log(codeGenerator('GH', 5));
