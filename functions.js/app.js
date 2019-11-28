6623a3e327b60c58cf298b68b7781d5aed8c86d9
codesaitama <achanamosey@gmail.com>
2019-11-28 10:33:02 +0000

    Test

--- functions.js/app.js
+++ functions.js/app.js
@@ -14,3 +14,6 @@ console.log(moneyFormat(moneyNum, 'en', 2));
 
 // Code Generator...
 let codeGenerator = (str, len) => `${str}-${Math.random().toString(36).substr(2, len).toUpperCase()}`;
+
+
+// Test
