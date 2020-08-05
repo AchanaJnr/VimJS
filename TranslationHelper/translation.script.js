7a2374d7b865b86d9f39cb6142bbca406231f052
Emmanuel Achana <achanamosey@gmail.com>
2020-08-05 09:20:11 +0100

    Function to handle getting all the strings and logic to handle when to run that function.

--- TranslationHelper/translation.script.js
+++ TranslationHelper/translation.script.js
@@ -19,3 +19,24 @@ let renderTranslations = () => {
 };
 
 const langLocStorName = 'langHCMCustomerAdminLiteStrings';
+
+//if no languages found in storage, insert new one
+if (!localStorage.getItem(langLocStorName)) {
+    reloadLanguagesTranslations();
+} else {
+    AllStrings = JSON.parse(localStorage.getItem(langLocStorName));
+}
+
+function reloadLanguagesTranslations(){
+    $.getJSON(`${window.location.href.indexOf("localhost") > -1 ? `${window.location.origin}/api/` : `${window.location.origin}/hcm-manager/api/`}Master/GetAllLanguageTrans`, function (response) {
+        if (response.body) {
+
+            response = response.body.map(x => { return { id: x.id, code: x.code, en: x.english, fr: x.french, es: x.spanish, gr: x.german, pr: x.portuguese } });
+
+            localStorage.setItem(langLocStorName, JSON.stringify(response));
+            AllStrings = JSON.parse(localStorage.getItem(langLocStorName));
+            renderTranslations(); //run functions
+        }
+        
+    });
+}
