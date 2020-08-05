0d4b08a8932fcb812f2b7b31b7d42192bc1fd30e
Emmanuel Achana <achanamosey@gmail.com>
2020-08-05 09:15:13 +0100

    Added the toggler for the languages

---
+++ Data/TranslationHelper/translation.script.js
@@ -0,0 +1,12 @@
+let getLanguageOfBrowser = () => {
+    //This returns the first two characters of the browser language. This is because of the different locales. for example: en-US, en-GB are all en.
+    return `${navigator.language.charAt(0)}${navigator.language.charAt(1)}`;
+};
+
+let changeUserLanguage = (value) => {
+    let languageCode = value.split(' ').pop();
+    localStorage.setItem("lingua", languageCode);
+    location.reload(); //Refresh page to take effect
+};
+
+let AllStrings = "", selectedLingo = localStorage.getItem("lingua") || getLanguageOfBrowser();
