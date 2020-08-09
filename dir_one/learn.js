4c421927b0759ee565f0f0681d61c2b48eeb87b7
Emmanuel Achana <achanamosey@gmail.com>
2020-08-09 23:42:17 +0100

    Continue to the next iteration

--- dir_one/learn.js
+++ dir_one/learn.js
@@ -36,3 +36,11 @@ while (true) {
 
 }
 alert( 'Sum: ' + sum );
+
+for (let i = 0; i < 10; i++) {
+
+    // if true, skip the remaining part of the body
+    if (i % 2 == 0) continue;
+  
+    alert(i); // 1, then 3, 5, 7, 9
+}
