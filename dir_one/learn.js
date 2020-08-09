fa265140152afc0b1d0b6f0af1cd93d07c0a0574
Emmanuel Achana <achanamosey@gmail.com>
2020-08-09 23:41:27 +0100

    Skipping parts

--- dir_one/learn.js
+++ dir_one/learn.js
@@ -10,3 +10,38 @@ let width = null;
 let area = (height ?? 100) * (width ?? 50);
 
 alert(area); // 5000
+
+
+
+let i = 0;
+while (i < 3) { // shows 0, then 1, then 2
+  alert( i );
+  i++;
+}
+
+for (let i = 0; i < 3; i++) { // shows 0, then 1, then 2
+    alert(i);
+}
+
+
+let sum = 0;
+
+while (true) {
+
+  let value = +prompt("Enter a number", '');
+
+  if (!value) break; // (*)
+
+  sum += value;
+
+}
+alert( 'Sum: ' + sum );
+
+
+for (let i = 0; i < 10; i++) {
+
+    // if true, skip the remaining part of the body
+    if (i % 2 == 0) continue;
+  
+    alert(i); // 1, then 3, 5, 7, 9
+  }
