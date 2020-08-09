cb7eaf616c7039eafda81b1067166f684b4655e2
Emmanuel Achana <achanamosey@gmail.com>
2020-08-09 23:41:59 +0100

    Breaking loop

--- dir_one/learn.js
+++ dir_one/learn.js
@@ -36,12 +36,3 @@ while (true) {
 
 }
 alert( 'Sum: ' + sum );
-
-
-for (let i = 0; i < 10; i++) {
-
-    // if true, skip the remaining part of the body
-    if (i % 2 == 0) continue;
-  
-    alert(i); // 1, then 3, 5, 7, 9
-  }
