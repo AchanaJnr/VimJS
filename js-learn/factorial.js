1d79726d66e2344122e88f45642d7f9acef33ee6
Emmanuel Achana <achanamosey@gmail.com>
2020-08-21 20:23:00 +0100

    Added two function that produce the same results

--- js-learn/factorial.js
+++ js-learn/factorial.js
@@ -10,4 +10,26 @@ function factorial(x){
     else return 1;
 }
 
-console.log(factorial(10))
+function factorial2(n) {    // A function to compute factorials
+    let product = 1;       // Start with a product of 1
+    while(n > 1) {         // Repeat statements in {} while expr in () is true
+        product *= n;      // Shortcut for product = product * n;
+        n--;               // Shortcut for n = n - 1
+    }                      // End of loop
+    return product;        // Return the product
+}
+
+function factorial3(n) {   // Another version using a different loop
+    let i, product = 1;    // Start with 1
+    for(i=2; i <= n; i++)  // Automatically increment i from 2 up to n
+        product *= i;      // Do this each time. {} not needed for 1-line loops
+    return product;        // Return the factorial
+}
+
+
+
+console.log(factorial(6))             // => 720: 1*2*3*4*5*6
+
+console.log(factorial2(6))              // => 720: 1*2*3*4*5*6
+
+console.log(factorial3(5))             // => 120: 1*2*3*4*5
