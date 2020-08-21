67eb4d4538427765c4e1371b5902d60e5e16c5ca
Emmanuel Achana <achanamosey@gmail.com>
2020-08-21 12:49:20 +0100

    Added explanation for factorial

--- js-learn/factorial.js
+++ js-learn/factorial.js
@@ -1,3 +1,10 @@
+/**
+ * 
+ * @param {*} 
+ * Factorials are very simple things. They're just products, indicated by an exclamation mark. For instance, "four factorial" is written as "4!" and means 1×2×3×4 = 24. In general, 
+ * n! ("enn factorial") means the product of all the whole numbers from 1 to n; that is, n!
+ */
+
 function factorial(x){
     if(x > 1) return x * factorial(x - 1);
     else return 1;
