642d48e756acf1358191b27d14765e03f3bd47b2
Emmanuel Achana <achanamosey@gmail.com>
2020-08-28 11:44:51 +0100

    Added a method to get the school's star rating depending on the year.

--- js-learn/class.test.js
+++ js-learn/class.test.js
@@ -6,5 +6,8 @@ class School{
         this.year = year;
     }
 
-  
+    rateSchool(year){
+        return ((year >= 1900 && year <= 1949) ? '3' : (year >= 1950 && year <= 1999) ? '2' : (year >= 2000 && year <= new Date().getFullYear()) ? '1' : '0') + 'Star Rating';
+    }
+
 }
