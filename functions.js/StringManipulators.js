283c60a16637a7a03125c77892f54fe0c6f6c81a
Emmanuel Achana <achanamosey@gmail.com>
2021-03-23 13:31:30 +0000

    Function to generate Unique IDs.

--- functions.js/StringManipulators.js
+++ functions.js/StringManipulators.js
@@ -3,3 +3,13 @@ function ucwords(str) {
     return str ? str.toLowerCase()
         .replace(/(^([a-zA-Z\p{M}]))|([ -][a-zA-Z\p{M}])/g, (f) => f.toUpperCase()) : '';
 }
+
+//generate bankIds when creating new banks
+function generateUniqueID() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 15; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+}
