1d87dc64199ddd7135f534858fcfa4a063ec1a24
Emmanuel Achana <achanamosey@gmail.com>
2021-03-23 13:25:26 +0000

    Function to help remove characters for tags etc.

---
+++ functions.js/XSSFilter.js
@@ -0,0 +1,16 @@
+/**
+ * Chidey abin banza.
+ * @param {*} toOutput 
+ */
+ function szPreserver(toOutput) {
+    if (typeof (toOutput) === 'string') {
+        return toOutput.replace(/\&/g, '&amp;')
+            .replace(/\</g, '&lt;')
+            .replace(/\>/g, '&gt;')
+            .replace(/\"/g, '&quot;')
+            .replace(/\'/g, '&#x27');
+        //.replace(/\//g, '&#x2F');
+    } else {
+        return toOutput
+    }
+}
