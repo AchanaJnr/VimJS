42157e41a330cfc27ac220fae79cb6a4f43cb45b
codesaitama <achanamosey@gmail.com>
2021-03-25 23:59:50 +0000

    Added two new files.

---
+++ samples/geolocation.js
@@ -0,0 +1,8 @@
+console.log(navigator.geolocation.getCurrentPosition(showPosition));
+
+    function showPosition(position) {
+      console.log("Latitude: " + position.coords.latitude +
+      "<br>Longitude: " + position.coords.longitude);
+    };
+    
+    console.log(true);
