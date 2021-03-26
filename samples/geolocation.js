1d604282cb44284c76a6e5b4edab7838bf424baa
codesaitama <achanamosey@gmail.com>
2021-03-26 23:33:02 +0000

    Log test.

--- samples/geolocation.js
+++ samples/geolocation.js
@@ -1,6 +1,23 @@
-console.log(navigator.geolocation.getCurrentPosition(showPosition));
+navigator.geolocation.getCurrentPosition(getCoor, errorCoor, {maximumAge:60000, timeout:5000, enableHighAccuracy:true});
 
-    function showPosition(position) {
-      console.log("Latitude: " + position.coords.latitude +
-      "<br>Longitude: " + position.coords.longitude);
-    };
+function getCoor(data){
+  console.log(data);
+};
+function errorCoor(err){
+  console.log(err);
+};
+
+//Dummy one, which will result in a working next statement.
+navigator.geolocation.getCurrentPosition(function () {}, function () {}, {});
+//The working next statement.
+navigator.geolocation.getCurrentPosition(function (position) {
+    //Your code here
+    console.log(position);
+}, function (e) {
+    //Your error handling here
+    console.log(e);
+}, {
+    enableHighAccuracy: true
+});
+
+console.log (true)
